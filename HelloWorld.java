1234f15333b21af11f02f34395fe6aca5ff40a79
Effect <philip.lane15@gmail.com>
2025-02-10 20:47:36 -0500

    Add files via upload
    Added Hello Alice message.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,10 @@
 
 public class HelloWorld {
     public static void main(String[] args) {
+        System.out.println("Hi Bob");
         System.out.println("Hello World!");
+        System.out.println("Goodbye Alice!");
+        System.out.println("Hi Alice");
     }
+
 }
